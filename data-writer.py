ebd71e043f08ab6e238ffd56ac68a6f70aab3a4f
Oliver Garnica <ogarnica@umich.edu>
2025-04-08 16:38:12 -0400

    plan for data-writer

---
+++ data-writer.py
@@ -0,0 +1,6 @@
+"""
+will start by gathering the data from finnhub.io api
+This will parse the data into a CSV file containing the insider traders name, and what trades they did and on what days they did them, pnl, etc.
+Then, using marketstack, we will access the data of the specific stocks that each trader traded and comparer there pnl to the movement of those in that day
+Here we will make a data table that contains the  market 
+"""
