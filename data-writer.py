cbf079b5737a41c5e3e9421bed9dd8e863569363
Oliver Garnica <ogarnica@umich.edu>
2025-04-08 16:57:27 -0400

    Basic shell functs

--- data-writer.py
+++ data-writer.py
@@ -1,6 +1,15 @@
-"""
-will start by gathering the data from finnhub.io api
-This will parse the data into a CSV file containing the insider traders name, and what trades they did and on what days they did them, pnl, etc.
-Then, using marketstack, we will access the data of the specific stocks that each trader traded and comparer there pnl to the movement of those in that day
-Here we will make a data table that contains the  market 
-"""
+import requests
+import sqlite3
+
+def load_config():
+    """
+    Will load api keys and configuration
+    """
+    pass
+
+def market_stack():
+    pass
+
+def clean_market_stack():
+    pass
+
