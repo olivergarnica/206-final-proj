86f48bff2a364ae69f0cc8d2ad5cd9747072f521
Kibrom Bereket <kibromandeber@gmail.com>
2025-04-16 17:15:11 -0400

    Processed data, added insider api key, created database all_data.db

--- main.py
+++ main.py
@@ -1,14 +1,63 @@
 # Group Name: Kappa
 # Group Members: Kanishk, Kibrom, Oliver
 
-import os
-import json
-import requests
-import unittest
-import sqlite3
 from insider import fetch_finnhub_transactions, stock_tickers, FH_KEY
 from databases import APIdatamanager
+from econ import fetch_econdb_data
+from marketstack import fetch_marketstack_data
 import time
 
 def main():
-    db_manager = APIdatamanager(db_path="")
+    print("Starting data collection...")
+
+    # Initialize db manager
+    db_manager = APIdatamanager(db_path="all_data.db")
+
+    try:
+        # Process finnhab insider data
+        print("\nFetching insider trading data...")
+        for cap_level in stock_tickers:
+            print(f"Processing {cap_level} cap stocks:")
+            for symbol in stock_tickers[cap_level]:
+                print(f" -- {symbol}", end=" ")
+                
+                try:
+                    insider_data = fetch_finnhub_transactions(symbol, FH_KEY)
+                    db_manager.insert_finnhub_data(insider_data)
+                    print("Success")
+                    time.sleep(1)
+                except Exception as e:
+                     print(f"Failure (Error: {str(e)})")
+        
+        # Process marketstack data
+        print("\nFetching stock market data...")
+        for cap_level in stock_tickers:
+            print(f"Processing {cap_level} cap stocks:")
+            for symbol in stock_tickers[cap_level]:
+                print(f" -- {symbol}", end=" ")
+                
+                try:
+                    market_data = fetch_marketstack_data(symbol, FH_KEY)
+                    db_manager.insert_marketstack_data(market_data)
+                    print("Success")
+                    time.sleep(1)
+                except Exception as e:
+                     print(f"Failure (Error: {str(e)})")
+        
+        # Process economic data
+        print("\nFetching economic indicators...")
+        try:
+            econ_data = fetch_econdb_data()
+            db_manager.insert_econdb_data(econ_data)
+            print("Economic data saved successfully!")
+        except Exception as e:
+            print(f"Failed to save economic data: {str(e)}")
+
+        print("\nAll data processed successfully!")
+        print("Database saved to: all_data.db")
+
+    finally:
+        db_manager.close()
+
+if __name__ == "__main__":
+    main() 
