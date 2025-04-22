d6440292aba0d4fe41d338aa29da81ffa2da463f
Oliver Garnica <ogarnica@umich.edu>
2025-04-22 13:16:55 -0400

    deleted run_analysis.py

--- run_analysis.py
+++
@@ -1,8 +0,0 @@
-from analysis import *
-
-def run_analysis():
-    calculate_and_write_pnl(db_path="all_data.db", output_path="trade_pnls.txt")
-    analyze_pnls_by_company(pnl_file_path="trade_pnls.txt", output_path="company_pnl_summary.txt")
-
-if __name__ == "__main__":
-    run_analysis()
